f5d1d10ca431d92f756b76cb29454083525e7248
Jake Rickard <jakelrickard@gmail.com>
2019-02-07 02:37:50 +0000

    creating test

---
+++ spec/helpers/grams_helper_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe GramsHelper, type: :helper do
+# Specs in this file have access to a helper object that includes
+# the GramsHelper. For example:
+#
+# describe GramsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+
+end
